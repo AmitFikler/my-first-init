3afde22d3466d7f88bc57edf8d8d087cae342efd
AmitFikler <amitfikler@gmail.com>
2021-10-18 14:13:21 +0300

    adding timeFromAToB function

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function timeToNow([year,moute,day]){
     return moment([year,moute-1,day]).toNow(true)
 }
 
+function timeFromAToB(dateA,dateB){
+    dateB = moment(dateB)
+    dateA = moment(dateA)
+    return dateA.to(dateB)
+}
