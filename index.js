7fe7a88c20fbcd9b4c5aeb589e6d0713d7bc295e
AmitFikler <amitfikler@gmail.com>
2021-10-18 14:25:07 +0300

    Realization of functions

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let moment = require('moment'); // require
 
-function timeToNow([year,moute,day]){
-    return moment([year,moute-1,day]).toNow(true)
+function timeToNow([year,month,day]){
+    return moment([year,month-1,day]).toNow(true)
 }
+timeToNow([1999,06,25]) //return 22 years
 
 function timeFromAToB(dateA,dateB){
     dateB = moment(dateB)
     dateA = moment(dateA)
     return dateA.to(dateB)
 }
+
+timeFromAToB([2021,10,10],[2021,10,18]) // in 8 days
 
 function daysInMonth(date, format){
     return moment(date,format).daysInMonth();
