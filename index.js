ac56ff1e7a9eb3acfe13f0d9d0cf70b10a3e3d76
AmitFikler <amitfikler@gmail.com>
2021-10-18 14:04:14 +0300

    adding timeToNow

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-import moment from 'moment';
-moment().format();
+let moment = require('moment'); // require
+// console.log(moment().format()); //Today
+function timeToNow([year,moute,day]){
+    return moment([year,moute-1,day]).toNow(true)
+}
+
